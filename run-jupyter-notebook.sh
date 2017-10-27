f8008c9919894eebb297d19929d09ad09ea2b7db
Sam Hopkins <sam@crowdprocess.com>
2017-10-27 12:07:52 +0100

    Update run-jupyter-notebook.sh

--- run-jupyter-notebook.sh
+++ run-jupyter-notebook.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run  --volume `pwd`:/root/units --workdir /root/units -it --rm -p 127.0.0.1:8888:8888 ldssa/lisbon.ai:1
+docker run  --volume `pwd`:/root/units --workdir /root/units -it --rm -p 127.0.0.1:8888:8888 ldssa/lisbon.ai:2
